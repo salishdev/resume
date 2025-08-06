29e6aa17721e4b82ff10876189abbfa7785e2f0c
Jeremy Jones <jeremy@salish.dev>
2025-08-06 07:19:16 -0700

    cleanup files

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,5 @@ docker build -t resume-builder .
 
 CID=$(docker create resume-builder)
 docker cp "$CID:/data/resume.pdf" resume.pdf
-docker cp "$CID:/data/resume.tex" latex/resume.tex
+# docker cp "$CID:/data/resume.tex" latex/resume.tex
 docker rm "$CID"
